134a07350b46a106099e9476f693574fc2bf5a1c
Lucas Lai <llai@xogrp.com>
2015-04-30 15:26:55 +0800

    order show

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+  default_scope { order('title') }
   has_many :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
   validates :title, :description, :image_url, :presence => {:message => " not be blank"}
